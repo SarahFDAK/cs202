f155c2f207a1302116b99c0659c0cedc60026f13
Sarah Carter <sarah.c@fdak.net>
2020-01-19 11:47:56 -0900

    Added absolute zero check to main before calling cpp_ftoc.

--- hw0/hw0/main.cpp
+++ hw0/hw0/main.cpp
@@ -20,10 +20,13 @@ double cpp_ftoc(const char* str){
     std::istringstream convert(str);
     double f;
     convert >> f;
-    cout << f << endl;
     double c = (f - 32.0) / 1.8;
     return c;
 }
+
+//double c_ctof(const char* str){
+//
+//}
 
 int main(int argc, const char * argv[]) {
     std::vector <std::string> args;
@@ -32,6 +35,13 @@ int main(int argc, const char * argv[]) {
         args.push_back(argv[index]);
     }
     if(argc >= 2 and args[1] == "--ftoc"){
+        std::istringstream check(args[2].c_str());
+        double z;
+        check >> z;
+        if(z <= -459.67){
+            cout << "That temperature does not exist." << endl;
+            return 0;
+        }
         cout << args[2] << " Fahrenheit = " << cpp_ftoc(args[2].c_str()) << " Celcius." << endl;
     }
     return 0;
