1d644918ede9466a04d6fe3e0697a3a92a03985d
Sarah Carter <sarah.c@fdak.net>
2020-01-18 15:10:16 -0900

    Created hw0 project and set Xcode to C++ 17

---
+++ hw0/hw0/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hw0
+//
+//  Created by Sarah Carter on 1/18/20.
+//  Copyright © 2020 Sarah Carter. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
