f4e02bf1bf01faf42d3acff443b19a86d9a51109
Sarah Carter <sarah.c@fdak.net>
2020-01-18 16:47:01 -0900

    Got clarification on command line with Xcode and created ftoc function.

--- hw0/hw0/main.cpp
+++ hw0/hw0/main.cpp
@@ -8,12 +8,30 @@
 
 #include <iostream>
 #include <vector>
+#include <sstream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+double cpp_ftoc(const char* str){
+    double F = 0.0;
+    std::istringstream convert(str);
+    convert >> F;
+    double C = (F - 32) * (5/9);
+    return C;
+}
 
 int main(int argc, const char * argv[]) {
     std::vector <std::string> args;
+
     for(int index = 0; index < argc; index++){
         args.push_back(argv[index]);
-        std::cout << args[index] << std::endl;
+    }
+    if(argc >= 2 and args[1] == "--ftoc"){
+        cout << args[2] << "Fahrenheit = " << cpp_ftoc(args[2]) << " Celcius." << endl;
     }
     return 0;
 }
+
+
