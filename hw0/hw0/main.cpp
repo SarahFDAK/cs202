32dab77b9bdfc54e41f4099cf880966d2b23d71e
Sarah Carter <sarah.c@fdak.net>
2020-01-19 12:26:56 -0900

    Added ctof input check (allowing for --ctof, -ctof, and ctof), and finished comments.

--- hw0/hw0/main.cpp
+++ hw0/hw0/main.cpp
@@ -47,29 +47,39 @@ int main(int argc, const char * argv[]) {
         args.push_back(argv[index]);
     }
     
-    //Check if user input
-    if(argc >= 2 && (args[1] == "--ftoc" || args[1] == "-ftoc" || args[1] ==
-                     "ftoc")){
+    //Check if user input specifies ftoc
+    if(argc >= 2 && (args[1] == "--ftoc" || args[1] == "-ftoc" || args[1]
+                     == "ftoc")){
+        //Cast input to a double to check if the temperature is below
+        //absolute zero.
         std::istringstream checkc(args[2].c_str());
         double azf;
         checkc >> azf;
+        //If the temp is too cold, exit the program.
         if(azf < -459.67){
             cout << "That temperature does not exist." << endl;
             return 0;
         }
-        cout << args[2] << " Fahrenheit = " << cpp_ftoc(args[2].c_str()) << " Celcius."
-        << endl;
+        //Print the results for the user.
+        cout << args[2] << " Fahrenheit = " << cpp_ftoc(args[2].c_str())
+        << " Celcius." << endl;
     }
-    if(argc >= 2 && args[1] == "--ctof"){
+    
+    //Check if user specifies ctof
+    if(argc >= 2 && (args[1] == "--ctof" || args[1] == "-ctof" || args[1]
+                     == "ctof")){
+        //Use istringstream to check temp existence
         std::istringstream checkc(args[2].c_str());
         double azc;
         checkc >> azc;
+        //If temp is less than absolute zero, exit the program.
         if(azc < -273.15){
             cout << "That temperature does not exist." << endl;
             return 0;
         }
-        cout << args[2] << " Celcius = " << c_ctof(args[2].c_str()) << " Fahrenheit."
-        << endl;
+        //Print the results for the user.
+        cout << args[2] << " Celcius = " << c_ctof(args[2].c_str())
+        << " Fahrenheit." << endl;
     }
     return 0;
 }
