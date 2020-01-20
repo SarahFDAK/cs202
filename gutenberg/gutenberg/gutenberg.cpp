404cd226aad0c20bd1d13a72435382e1efce310f
Sarah Carter <sarah.c@fdak.net>
2020-01-19 17:28:03 -0900

    Added error checking upon opening of the file in main

--- gutenberg/gutenberg/gutenberg.cpp
+++ gutenberg/gutenberg/gutenberg.cpp
@@ -18,26 +18,35 @@ using std::getline;
 using std::istream;
 using std::ifstream;
 
+//Check if the line read from the book is blank, i.e. the end of
+//a paragraph.
 bool blank(const string &line){
     return std::all_of(line.begin(), line.end(), isspace);
 }
 
+//Pass the selected book file to be read, line by line.
 string paragraph(istream &in){
     string line, concat;
     int linecount = 0;
+    
     while(getline(in, line)){
         linecount++;
+        //Check if the line is blank, and then how many lines have been read
         if(blank(line)){
+            //If it hasn't read at least 20 lines, continue the loop.
+            //Otherwise, break.
             if(linecount <= 20)
                 continue;
             break;
         }
+        //Concatenate the lines into a new string object to be printed.
             else
                 concat += (line + "\n");
     }
     return concat;
 }
 
+//Give the user the choice of 4 books to see an exerpt from.
 string choice(){
     int choice;
     cout << "Select which book you want to see part of:\n"
@@ -65,7 +74,16 @@ string choice(){
 }
 
 int main(int argc, const char * argv[]) {
+    //Open the file chosen by the user
     ifstream file(choice());
+    if(!file){
+        if(eof())
+            cout << "That is the end of the book." << endl;
+        else{
+            cout << "Error opening file." << endl;
+            return 9;
+        }
+    }
     string text = paragraph(file);
     file.close();
     
