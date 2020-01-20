d80e898009938ee98a46966ba45c57f5add60cae
Sarah Carter <sarah.c@fdak.net>
2020-01-19 16:09:40 -0900

    Modified switch statement in the choice function to actually work - it lacked semi-colons and I was not necessarily returning anything.

--- gutenberg/gutenberg/gutenberg.cpp
+++ gutenberg/gutenberg/gutenberg.cpp
@@ -15,10 +15,11 @@ using std::cout;
 using std::endl;
 using std::getline;
 using std::istream;
+using std::ifstream;
 
 string paragraph(const istream &in){
     string line, concat;
-    while(getline(cin, line)){
+    while(getline(std::cin, line)){
         if(line.empty())
             break;
         concat += (line + "\n");
@@ -32,22 +33,36 @@ string choice(){
     <<"1 - A Tale of Two Cities by Charles Dickens\n"
     <<"2 - Pride And Prejudice by Jane Austen\n"
     <<"3 - The Raven by Edgar Allen Poe\n"
-    <<"4 - MacBeth by Shakespeare" << endl;
-    switch(std::cin(choice)){
+    <<"4 - MacBeth by Shakespeare"
+    <<"If you make no choice, the automatic selection is The Brothers Grimm"
+    << endl;
+    std::cin >> choice;
+    string book;
+    switch(choice){
         case 1:
-            return "98-0.txt"
+            book = "98-0.txt";
+            break;
         case 2:
-            return "1342-0.txt"
+            book = "1342-0.txt";
+            break;
         case 3:
-            return "pg17192.txt"
+            book = "pg17192.txt";
+            break;
         case 4:
-            return "pg2264.txt"
+            book = "pg2264.txt";
+            break;
         default:
+            book = "52521-0.txt";
             break;
     }
+    return book;
 }
 
 int main(int argc, const char * argv[]) {
+    ifstream file(choice());
+    string text = paragraph(file);
+    file.close();
     
+    cout << text << endl;
     return 0;
 }
