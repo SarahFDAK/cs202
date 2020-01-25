7bb98ae502f16b8593c7aeb2c4146e16b312644d
Sarah Carter <sarah.c@fdak.net>
2020-01-25 13:17:40 -0900

    Created StopWatch header and cpp files

---
+++ hw01/hw01/StopWatch.cpp
@@ -0,0 +1,9 @@
+//
+//  StopWatch.cpp
+//  hw01
+//
+//  Created by Sarah Carter on 1/25/20.
+//  Copyright © 2020 Sarah Carter. All rights reserved.
+//
+
+#include "StopWatch.hpp"
---
+++ hw01/hw01/StopWatch.hpp
@@ -0,0 +1,16 @@
+//
+//  StopWatch.hpp
+//  hw01
+//
+//  Created by Sarah Carter on 1/25/20.
+//  Copyright © 2020 Sarah Carter. All rights reserved.
+//
+
+#ifndef StopWatch_hpp
+#define StopWatch_hpp
+
+#include <stdio.h>
+#include <chrono>
+
+
+#endif /* StopWatch_hpp */
