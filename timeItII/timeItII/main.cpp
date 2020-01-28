51be2eb7435db2f3b82a61b1c2702a3df11e07ff
Sarah Carter <sarah.c@fdak.net>
2020-01-27 20:13:53 -0900

    Created timeItII project and added StopWatch header and source from hw01 project.

---
+++ timeItII/timeItII/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  timeItII
+//
+//  Created by Sarah Carter on 1/27/20.
+//  Copyright © 2020 Sarah Carter. All rights reserved.
+//
+#include <fstream>
+
+#include "StopWatch.hpp"
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
