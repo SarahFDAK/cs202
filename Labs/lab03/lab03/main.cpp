53cbbbf4b2313156e5e02d611580f4be39ecf176
Sarah Carter <sarah.c@fdak.net>
2020-02-02 14:56:12 -0900

    Completed lab03 code and tested - I will now add comments

--- Labs/lab03/lab03/main.cpp
+++ Labs/lab03/lab03/main.cpp
@@ -12,10 +12,26 @@
 using std::cout;
 using std::endl;
 
+
+
 int main(int argc, const char * argv[]) {
-    cout << "Creating raw pointer: " << endl;
+    
+    cout << "Creating raw pointer:\n";
     LabClass* myPtr = new LabClass;
-    cout << myPtr << endl;
-    myPtr -> ~LabClass();
+    cout << myPtr -> getCopy() << endl;
+    
+    cout << "Create unique pointer:\n";
+    auto myPtr2 = std::make_unique<LabClass>(5);
+    cout << myPtr2 << " unique pointer address\n\n";
+    
+    cout << "Transferring unique pointer ownership:\n";
+    auto myPtr3 = std::move(myPtr2);
+    cout << myPtr3 << " transferred ownership address\n\n";
+    
+    cout << "Create shared pointer:\n";
+    auto mySPtr = std::make_shared<LabClass>(12);
+    std::shared_ptr<LabClass> mySPtr2(mySPtr);
+    cout << mySPtr << " is first shared pointer address and\n" <<
+    mySPtr << " is the second shared pointer address\n\n";
     return 0;
 }
