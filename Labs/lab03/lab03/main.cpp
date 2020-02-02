b2eda76ffe2004cc039f5dd256fac38d14125c67
Sarah Carter <sarah.c@fdak.net>
2020-02-02 14:59:40 -0900

    Commented lab03

--- Labs/lab03/lab03/main.cpp
+++ Labs/lab03/lab03/main.cpp
@@ -19,6 +19,8 @@ int main(int argc, const char * argv[]) {
     cout << "Creating raw pointer:\n";
     LabClass* myPtr = new LabClass;
     cout << myPtr -> getCopy() << endl;
+    //You can see that this pointer is created, but I don't see anywhere that it is
+    //de-allocated. If I manually call the destructor here, it will be.
     
     cout << "Create unique pointer:\n";
     auto myPtr2 = std::make_unique<LabClass>(5);
@@ -27,11 +29,18 @@ int main(int argc, const char * argv[]) {
     cout << "Transferring unique pointer ownership:\n";
     auto myPtr3 = std::move(myPtr2);
     cout << myPtr3 << " transferred ownership address\n\n";
+    //Printing the first unique pointer address, moving it, and then printing the
+    //second shows that the ownership of the address was moved to the new pointer.
     
     cout << "Create shared pointer:\n";
     auto mySPtr = std::make_shared<LabClass>(12);
     std::shared_ptr<LabClass> mySPtr2(mySPtr);
     cout << mySPtr << " is first shared pointer address and\n" <<
     mySPtr << " is the second shared pointer address\n\n";
+    //These printed lines show that the original shared address was copied to the
+    //second shared address. I am not sure the unique pointer from before was
+    //de-allocated, unless the one destructor line is for the unique pointer and
+    //the other is for the two shared pointers de-allocating at once.
+    
     return 0;
 }
