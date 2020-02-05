3f4add84b7ec67ba66807b4f540610654b24f558
Sarah Carter <sarah.c@fdak.net>
2020-02-04 18:55:10 -0900

    Created char array and char* s

---
+++ lowerCase/lowerCase/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  lowerCase
+//
+//  Created by Sarah Carter on 2/4/20.
+//  Copyright © 2020 Sarah Carter. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    char str[13] = "HelLo ThErE!";
+    char* s;
+    s = str;	
+    return 0;
+}
