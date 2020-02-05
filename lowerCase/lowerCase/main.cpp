43ab319e8765aefa9b2474fc7efb71f635b5d9f6
Sarah Carter <sarah.c@fdak.net>
2020-02-04 18:56:20 -0900

    Added to lower function to lowerCase project

--- lowerCase/lowerCase/main.cpp
+++ lowerCase/lowerCase/main.cpp
@@ -8,9 +8,19 @@
 
 #include <iostream>
 
+void to_lower(char* s){
+    for(int i = 0; s[i] != 0; i++){
+        if(s[i] >= 'A' && s[i] <= 'Z'){
+            s[i]+=32;
+            std::cout << s[i];
+        }
+    }
+}
+
+
 int main(int argc, const char * argv[]) {
     char str[13] = "HelLo ThErE!";
     char* s;
-    s = str;	
+    s = str;
     return 0;
 }
