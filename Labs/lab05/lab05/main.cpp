33b038a6cfd937c4cefee684ce3e5896a00c2214
Sarah Carter <sarah.c@fdak.net>
2020-02-11 11:25:06 -0900

    Completed lab05

--- Labs/lab05/lab05/main.cpp
+++ Labs/lab05/lab05/main.cpp
@@ -7,9 +7,37 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <vector>
+
+using std::ifstream;
+using std::cout;
+using std::endl;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int read = 0;
+    int count = 0;
+    int sum = 0;
+    std::vector<int> holder;
+    ifstream fin("data.dat", std::ios::binary);
+    if(!fin){
+        if(fin.eof()){
+            cout << "End of file" << endl;
+            return 0;
+        }
+        else{
+            cout << "Error opening file" << endl;
+            return 9;
+        }
+    }
+    fin.seekg(0,std::ios_base::seekdir::end);
+    count =  fin.tellg()/sizeof(int);
+    fin.seekg(0,std::ios_base::seekdir::beg);
+    for(int i = 0; i < count; i++){
+        fin.read(reinterpret_cast<char*>(&read), sizeof(read));
+        holder.push_back(read);
+        sum += holder[i];
+    }
+    cout << count << " integers,\nTotal: " << sum << "\nAverage: " << static_cast<double>(sum)/count << endl;
     return 0;
 }
