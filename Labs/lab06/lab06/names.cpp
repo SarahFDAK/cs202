ab9512cbfe4e5e6677f3c1622b500f7d75cd7d57
Sarah Carter <sarah.c@fdak.net>
2020-02-13 10:28:22 -0900

    Created lab06 and names source and header files

---
+++ Labs/lab06/lab06/names.cpp
@@ -0,0 +1,9 @@
+//
+//  names.cpp
+//  lab06
+//
+//  Created by Sarah Carter on 2/13/20.
+//  Copyright © 2020 Sarah Carter. All rights reserved.
+//
+
+#include "names.hpp"
---
+++ Labs/lab06/lab06/names.hpp
@@ -0,0 +1,14 @@
+//
+//  names.hpp
+//  lab06
+//
+//  Created by Sarah Carter on 2/13/20.
+//  Copyright © 2020 Sarah Carter. All rights reserved.
+//
+
+#ifndef names_hpp
+#define names_hpp
+
+#include <stdio.h>
+
+#endif /* names_hpp */
