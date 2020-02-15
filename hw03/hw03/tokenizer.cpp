ab4ae881c2ead7c7fbfbe120ed9d7ba6e4020049
Sarah Carter <sarah.c@fdak.net>
2020-02-15 12:48:17 -0900

    Define ReadTokens functions in tokenizer source file

--- hw03/hw03/tokenizer.cpp
+++ hw03/hw03/tokenizer.cpp
@@ -7,10 +7,33 @@
 //
 
 #include <sstream>
-#include <fstream>
 
 #include "tokenizer.hpp"
 
 bool LineToTokens(const std::string& line, std::vector<std::string>& tokens){
+    return true;
+}
+bool ReadLine(std::istream& is, std::vector<std::string>& tokens,
+              std::vector<std::pair<int, int>>& linecols){
+    std::string textLine;
+    int lines = 0;
+    int columns = 0;
+    if(!is){
+        if(is.eof()){
+            std::cout << "That's the end!" << std::endl;
+            return false;
+        }
+        return false;
+    }
+    while(is >> textLine){
+        ++lines;
+        ++columns;
+        tokens.push_back(textLine);
+        linecols.push_back(std::make_pair(lines, columns));
+    }
+    return true;
+}
+void PrintTokens(std::ostream& os, const std::vector<std::string>& tokens,
+                 const std::vector<std::pair<int, int>>& linecols){
     
 }
