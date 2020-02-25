8a5c7fda788ff4d3703ff092340b9521106b2cf2
Sarah Carter <sarah.c@fdak.net>
2020-02-24 21:12:39 -0900

    Created wumpHunt header and source files

---
+++ hw04Wumpus/hw04Wumpus/wumpHunt.cpp
@@ -0,0 +1,9 @@
+//
+//  wumpHunt.cpp
+//  hw04Wumpus
+//
+//  Created by Sarah Carter on 2/24/20.
+//  Copyright © 2020 Sarah Carter. All rights reserved.
+//
+
+#include "wumpHunt.hpp"
---
+++ hw04Wumpus/hw04Wumpus/wumpHunt.hpp
@@ -0,0 +1,14 @@
+//
+//  wumpHunt.hpp
+//  hw04Wumpus
+//
+//  Created by Sarah Carter on 2/24/20.
+//  Copyright © 2020 Sarah Carter. All rights reserved.
+//
+
+#ifndef wumpHunt_hpp
+#define wumpHunt_hpp
+
+#include <stdio.h>
+
+#endif /* wumpHunt_hpp */
