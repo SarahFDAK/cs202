e0b064174087e62a532c26568642eb59e53e31b6
Sarah Carter <sarah.c@fdak.net>
2020-02-27 11:23:41 -0900

    Push static library I made from StopWatch

---
+++ Labs/labglfw/labglfw/MyGraphicalLibrary.cpp
@@ -0,0 +1,9 @@
+//
+//  MyGraphicalLibrary.cpp
+//  labglfw
+//
+//  Created by Sarah Carter on 2/25/20.
+//  Copyright © 2020 Sarah Carter. All rights reserved.
+//
+
+#include "MyGraphicalLibrary.hpp"
---
+++ Labs/labglfw/labglfw/MyGraphicalLibrary.hpp
@@ -0,0 +1,14 @@
+//
+//  MyGraphicalLibrary.hpp
+//  labglfw
+//
+//  Created by Sarah Carter on 2/25/20.
+//  Copyright © 2020 Sarah Carter. All rights reserved.
+//
+
+#ifndef MyGraphicalLibrary_hpp
+#define MyGraphicalLibrary_hpp
+
+#include <stdio.h>
+
+#endif /* MyGraphicalLibrary_hpp */
