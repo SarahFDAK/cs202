d713f554c630790515ece3df06084e9c9286183b
Sarah Carter <sarah.c@fdak.net>
2020-03-03 20:59:57 -0900

    Created test in main.cpp. Let's see if I've failed.

--- hw04-SpeedCode/hw04-SpeedCode/main.cpp
+++ hw04-SpeedCode/hw04-SpeedCode/main.cpp
@@ -6,10 +6,14 @@
 //  Copyright © 2020 Sarah Carter. All rights reserved.
 //
 
-#include <iostream>
+#include "SpeedCodeClasses.hpp"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    Streams myStream;
+    std::string myInput;
+    std::getline(std::cin, myInput);
+    std::istringstream iss(myInput);
+    myStream.setStream(iss);
+    myStream.Output();
     return 0;
 }
