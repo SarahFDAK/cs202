46899ccf3d4b3f6b39c23b0d26964b2aeb57daaa
Sarah Carter <sarah.c@fdak.net>
2020-03-04 19:18:56 -0900

    Added Containers object to main; wrote code to fill map.

--- hw04-SpeedCode/hw04-SpeedCode/main.cpp
+++ hw04-SpeedCode/hw04-SpeedCode/main.cpp
@@ -15,5 +15,19 @@ int main(int argc, const char * argv[]) {
     std::istringstream iss(myInput);
     myStream.setStream(iss);
     myStream.Output();
+    
+    Containers myContainer;
+    int myEntryNum = 0;
+    std::cout << "How many entries do you want to make?\n";
+    while(!std::cin >> myEntryNum){
+        std::cout << "Please enter an integer: \n";
+        std::cin.ignore();
+    }
+    myContainer.setEntries(myEntryNum);
+    for(int i = 0; i < myEntryNum; i++){
+        std::cout << "Word entry " << i << ": ";
+        std::getline(std::cin, myInput);
+        myContainer.addWords(i, myInput);
+    }
     return 0;
 }
