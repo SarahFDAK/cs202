325dd7e6b5b5c093a25def08e8f12e4e7de6c4c4
Sarah Carter <sarah.c@fdak.net>
2020-03-08 10:42:58 -0800

    Re-created Cave class

--- hw04Wumpus/hw04Wumpus/wumpHunt.hpp
+++ hw04Wumpus/hw04Wumpus/wumpHunt.hpp
@@ -13,9 +13,54 @@
 #include <map>
 #include <vector>
 
-std::vector<int> numbers {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
-    18, 19, 20};
-std::map<char, int> rooms;
+//Cave map:
+//Room  Neighbors
+//A     B, E, F
+//B     A, C, H
+//C     B, D, J
+//D     C, E, L
+//E     A, D, N
+//F     A, G, O
+//G     F, H, P
+//H     B, G, I
+//I     H, J, Q
+//J     C, I, K
+//K     J, L, R
+//L     D, K, M
+//M     L, N, S
+//N     E, M, O
+//O     F, N, T
+//P     G, Q, T
+//Q     I, P, R
+//R     K, Q, S
+//S     M, R, T
+//T     O, P, S
+
+class Cave {
+    //Cave's room number
+    int _roomNum;
+    //Neighboring rooms - dating myself by naming the neighbor after
+    //Home Improvement. Perhaps the neighbor is a volleyball...
+    char _wilson1;
+    char _wilson2;
+    char _wilson3;
+    
+public:
+    Cave();
+    Cave(int _roomNum);
+    
+    void setWilson1(const Cave& cave);
+    void setWilson2(const Cave& cave);
+    void setWilson3(const Cave& cave);
+    
+    int getRoom() const;
+    char getWilson1() const;
+    char getWilson2() const;
+    char getWilson3() const;
+};
+
+std::vector<int> rooms { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+    'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T' };
 
 
 #endif /* wumpHunt_hpp */
