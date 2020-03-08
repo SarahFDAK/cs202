9edb09ce8f4348895d56edf3cd71149b6455156f
Sarah Carter <sarah.c@fdak.net>
2020-03-08 13:33:56 -0800

    Defined Hazards member functions

--- hw04Wumpus/hw04Wumpus/Hazards.cpp
+++ hw04Wumpus/hw04Wumpus/Hazards.cpp
@@ -7,3 +7,29 @@
 //
 
 #include "Hazards.hpp"
+
+void Hazards::setBat1Room(const Cave& cave) {
+    _bat1Room = cave.getRoom();
+};
+void Hazards::setBat2Room(const Cave& cave) {
+    _bat2Room = cave.getRoom();
+};
+void Hazards::setPit1Room(const Cave& cave) {
+    _pit1Room = cave.getRoom();
+};
+void Hazards::setPit2Room(const Cave& cave) {
+    _pit2Room = cave.getRoom();
+};
+
+int Hazards::getBat1Room() const {
+    return _bat1Room;
+};
+int Hazards::getBat2Room() const {
+    return _bat2Room;
+};
+int Hazards::getPit1Room() const {
+    return _pit1Room;
+};
+int Hazards::getPit2Room() const {
+    return _pit2Room;
+};
