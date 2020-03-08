38532920a7b8678a69038761dfc3b5f34250f3d1
Sarah Carter <sarah.c@fdak.net>
2020-03-08 12:59:35 -0800

    Created class Bats in Hazards header

--- hw04Wumpus/hw04Wumpus/Hazards.hpp
+++ hw04Wumpus/hw04Wumpus/Hazards.hpp
@@ -10,5 +10,18 @@
 #define Hazards_hpp
 
 #include <stdio.h>
+#include "Cave.hpp"
+
+class Bats {
+    int _bat1Room = 0;
+    int _bat2Room = 0;
+  
+public:
+    void setBat1Room(const Cave& cave);
+    void setBat2Room(const Cave& cave);
+    
+    int getBat1Room() const;
+    int getBat2Room() const;
+};
 
 #endif /* Hazards_hpp */
