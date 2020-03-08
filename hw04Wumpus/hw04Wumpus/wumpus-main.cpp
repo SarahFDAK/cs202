78cd826509af3f29fd01289fc3d0e9242fdfe571
Sarah Carter <sarah.c@fdak.net>
2020-03-08 14:01:51 -0800

    Created random int generator

--- hw04Wumpus/hw04Wumpus/wumpus-main.cpp
+++ hw04Wumpus/hw04Wumpus/wumpus-main.cpp
@@ -7,8 +7,21 @@
 //
 
 #include <iostream>
+#include <random>
+
+using std::cout;
+using std::endl;
+
+int randNum(int a, int b){
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dist(a, b);
+    return dist(gen);
+}
+
 
 int main(int argc, const char * argv[]) {
+    
     
     return 0;
 }
