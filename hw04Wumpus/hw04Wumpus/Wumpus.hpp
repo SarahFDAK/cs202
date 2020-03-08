2232a13b74b5d54ee784ed2ed765a3937cc4925b
Sarah Carter <sarah.c@fdak.net>
2020-03-08 12:50:40 -0800

    Created Wumpus header and source

---
+++ hw04Wumpus/hw04Wumpus/Wumpus.hpp
@@ -0,0 +1,14 @@
+//
+//  Wumpus.hpp
+//  hw04Wumpus
+//
+//  Created by Sarah Carter on 3/8/20.
+//  Copyright © 2020 Sarah Carter. All rights reserved.
+//
+
+#ifndef Wumpus_hpp
+#define Wumpus_hpp
+
+#include <stdio.h>
+
+#endif /* Wumpus_hpp */
