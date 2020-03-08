a65504b13a02c8fb3037008a6265a45b2ed785f9
Sarah Carter <sarah.c@fdak.net>
2020-03-08 13:30:08 -0800

    Defined Explorer member functions

--- hw04Wumpus/hw04Wumpus/Explorer.cpp
+++ hw04Wumpus/hw04Wumpus/Explorer.cpp
@@ -6,4 +6,28 @@
 //  Copyright © 2020 Sarah Carter. All rights reserved.
 //
 
+
 #include "Explorer.hpp"
+
+void Explorer::setCurrentRoom(const Cave& cave) {
+    _yourRoom = cave.getRoom();
+};
+void Explorer::setLifeStat(const int alive){
+    _lifeStat = alive;
+};
+void Explorer::missed(const int usedArrow) {
+    if(_arrows > 1)
+        _arrows -= 1;
+    else
+        std::cout << "You are out of arrows. Better run away!\n";
+};
+
+int Explorer::getExplorerLife() const {
+    return _lifeStat;
+};
+int Explorer::getExplorerRoom() const {
+    return _yourRoom;
+};
+int Explorer::getArrowNum() const {
+    return _arrows;
+};
