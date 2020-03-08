bf97ce13b43528add83d32e8fec785c604f27d17
Sarah Carter <sarah.c@fdak.net>
2020-03-08 12:51:04 -0800

    Created Hazards header and source

---
+++ hw04Wumpus/hw04Wumpus/Hazards.hpp
@@ -0,0 +1,14 @@
+//
+//  Hazards.hpp
+//  hw04Wumpus
+//
+//  Created by Sarah Carter on 3/8/20.
+//  Copyright © 2020 Sarah Carter. All rights reserved.
+//
+
+#ifndef Hazards_hpp
+#define Hazards_hpp
+
+#include <stdio.h>
+
+#endif /* Hazards_hpp */
