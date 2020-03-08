d81e9cf1cf5d0664e85191a3b4e3a27c0516b74f
Sarah Carter <sarah.c@fdak.net>
2020-03-08 12:49:46 -0800

    Created Explorer header and source

---
+++ hw04Wumpus/hw04Wumpus/Explorer.cpp
@@ -0,0 +1,9 @@
+//
+//  Explorer.cpp
+//  hw04Wumpus
+//
+//  Created by Sarah Carter on 3/8/20.
+//  Copyright © 2020 Sarah Carter. All rights reserved.
+//
+
+#include "Explorer.hpp"
---
+++ hw04Wumpus/hw04Wumpus/Explorer.hpp
@@ -0,0 +1,14 @@
+//
+//  Explorer.hpp
+//  hw04Wumpus
+//
+//  Created by Sarah Carter on 3/8/20.
+//  Copyright © 2020 Sarah Carter. All rights reserved.
+//
+
+#ifndef Explorer_hpp
+#define Explorer_hpp
+
+#include <stdio.h>
+
+#endif /* Explorer_hpp */
