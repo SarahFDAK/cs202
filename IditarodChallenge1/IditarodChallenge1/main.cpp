d61ddb5ad576b9a6200eb78782751755c8a44137
Sarah Carter <sarah.c@fdak.net>
2020-04-05 16:39:24 -0800

    Write fibloop - non-recursive Fibonacci function

--- IditarodChallenge1/IditarodChallenge1/main.cpp
+++ IditarodChallenge1/IditarodChallenge1/main.cpp
@@ -15,8 +15,28 @@ unsigned int fib(unsigned int n){
     return fib(n-1) + fib(n-2);
 }
 
+unsigned int fib_loop(unsigned int n){
+    int count = 0;
+    int f = 0;
+    int f1 = 1;
+    int f2 = 0;
+    if(n < 2){
+        return n;
+    }
+    else{
+        while(count <= n-2){
+            f2 = f + f1;
+            f = f1;
+            f1 = f2;
+            count++;
+        }
+    }
+    return f2;
+}
+
 int main(int argc, const char * argv[]) {
-    unsigned int n = 6;
+    unsigned int n = 7;
     std::cout << fib(n) << "\n";
+    std::cout << fib_loop(n) << "\n";
     return 0;
 }
