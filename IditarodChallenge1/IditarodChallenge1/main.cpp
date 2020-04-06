ab2a28aa55db18d5321b0255a3cfc80abdc73b68
Sarah Carter <sarah.c@fdak.net>
2020-04-05 17:02:35 -0800

    Add code comments

--- IditarodChallenge1/IditarodChallenge1/main.cpp
+++ IditarodChallenge1/IditarodChallenge1/main.cpp
@@ -1,13 +1,15 @@
 //
 //  main.cpp
 //  IditarodChallenge1
-//
-//  Created by Sarah Carter on 4/5/20.
-//  Copyright © 2020 Sarah Carter. All rights reserved.
-//
+//  Sarah Carter
+//  4/5/20
+//  This code includes functions to report nth number in a Fibonacci sequence both
+//  recursively and non-recursively, and does that same for that number's factorial.
 
 #include <iostream>
 
+
+//Recursive Fibonacci function
 unsigned int fib(unsigned int n){
     if(n < 2){
         return n;
@@ -15,6 +17,7 @@ unsigned int fib(unsigned int n){
     return fib(n-1) + fib(n-2);
 }
 
+//Non-recursive Fibonacci function
 unsigned int fib_loop(unsigned int n){
     int count = 0;
     int f = 0;
@@ -34,6 +37,7 @@ unsigned int fib_loop(unsigned int n){
     return f2;
 }
 
+//Recursive factorial function
 unsigned int factorial(unsigned int n){
     if(n < 2){
         return 1;
@@ -41,6 +45,7 @@ unsigned int factorial(unsigned int n){
     return n * factorial(n-1);
 }
 
+//Non-recursive factorial function
 unsigned int factorial_loop(unsigned int n){
     int count = 2;
     int ft = 1;
@@ -57,13 +62,19 @@ unsigned int factorial_loop(unsigned int n){
 
 int main(int argc, const char * argv[]) {
     unsigned int n = 0;
+    //Get integer from user, set to n
     std::cout << "Enter a positive integer: \n";
     std::cin >> n;
+    
+    //Find nth number in Fibonacci sequence
     std::cout << "Fibonacci sequence at " << n << " is " << fib(n) << "\n";
     std::cout << "Non-recursive Fibonacci sequence function at "
         << n << " is " << fib_loop(n) << "\n";
+    
+    //Find n factorial
     std::cout << n << " factorial is " << factorial(n)<< "\n";
     std::cout << n << " factorial (non-recursive) is " << factorial_loop(n)
         << "\n";
+    
     return 0;
 }
