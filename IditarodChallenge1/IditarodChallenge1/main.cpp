e9486618e96115e80ab37b6e272b8cd05c6d355f
Sarah Carter <sarah.c@fdak.net>
2020-04-05 16:53:21 -0800

    Write factorial loop(n) - non-recursive factorial function

--- IditarodChallenge1/IditarodChallenge1/main.cpp
+++ IditarodChallenge1/IditarodChallenge1/main.cpp
@@ -41,6 +41,20 @@ unsigned int factorial(unsigned int n){
     return n * factorial(n-1);
 }
 
+unsigned int factorial_loop(unsigned int n){
+    int count = 2;
+    int ft = 1;
+    int ft1 = 1;
+    if(n < 2)
+        return 1;
+    while(count <= n){
+        ft1 = count * ft;
+        ft = ft1;
+        count++;
+    }
+    return ft1;
+}
+
 int main(int argc, const char * argv[]) {
     unsigned int n = 7;
     std::cout << "The number in the Fibonacci sequence at " << n << " is " <<
@@ -48,5 +62,7 @@ int main(int argc, const char * argv[]) {
     std::cout << "The number in the non-recursive Fibonacci sequence function at "
         << n << " is " << fib_loop(n) << "\n";
     std::cout << n << " factorial is " << factorial(n)<< "\n";
+    std::cout << n << " factorial (non-recursive) is " << factorial_loop(n)
+        << "\n";
     return 0;
 }
