b867ad763ea32044f921bf746dfc08e6b630fdc7
Sarah Carter <sarah.c@fdak.net>
2020-04-09 15:53:19 -0800

    Create CityNode class

---
+++ IditarodChallenge3/IditarodChallenge3/CityNode.cpp
@@ -0,0 +1,9 @@
+//
+//  CityNode.cpp
+//  IditarodChallenge3
+//
+//  Created by Sarah Carter on 4/9/20.
+//  Copyright © 2020 Sarah Carter. All rights reserved.
+//
+
+#include "CityNode.hpp"
---
+++ IditarodChallenge3/IditarodChallenge3/CityNode.hpp
@@ -0,0 +1,22 @@
+//
+//  CityNode.hpp
+//  IditarodChallenge3
+//
+//  Created by Sarah Carter on 4/9/20.
+//  Copyright © 2020 Sarah Carter. All rights reserved.
+//
+
+#ifndef CityNode_hpp
+#define CityNode_hpp
+
+#include <stdio.h>
+
+class CityNode {
+public:
+    CityNode ();
+    
+    ~CityNode ();
+};
+
+
+#endif /* CityNode_hpp */
