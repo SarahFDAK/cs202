30a15d7b3857c9e3d54561c0733262adcf30d1be
Sarah Carter <sarah.c@fdak.net>
2020-04-12 19:13:47 -0800

    A default base class object

--- Labs/Lab11/Lab11/main.cpp
+++ Labs/Lab11/Lab11/main.cpp
@@ -1,15 +1,40 @@
 //
-//  main.cpp
-//  Lab11
-//
-//  Created by Sarah Carter on 4/12/20.
-//  Copyright © 2020 Sarah Carter. All rights reserved.
+//  CS202 Lab11
+//  Sarah Carter
+//  Inheritance demonstration
 //
 
 #include <iostream>
 
+using std::cout;
+using std::endl;
+
+// [ ] A default base class object
+// [ ] A base class object using the one parameter constructor
+// [ ] A default derived class object
+// [ ] A derived class object using the two parameter constructor
+
+//Base class
+class Base {
+public:
+    Base()
+        : ccs_ { 1000 } {
+        cout << "Constructing a default base class object with a " << ccs_
+            << " cc engine" << endl;
+    }
+    
+    
+    ~Base() {
+        cout << "Destrucing the default base class object with a " << ccs_
+            << " cc engine." << endl;
+    }
+private:
+    int ccs_;
+};
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+            { Base b; }
+            cout << endl;
+            
     return 0;
 }
