af90ff0371554e1db31325bc7f635e7c9c49defd
Sarah Carter <sarah.c@fdak.net>
2020-04-18 18:19:51 -0800

    Create Lab12

---
+++ Labs/Lab12/Lab12/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab12
+//
+//  Created by Sarah Carter on 4/18/20.
+//  Copyright © 2020 Sarah Carter. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
