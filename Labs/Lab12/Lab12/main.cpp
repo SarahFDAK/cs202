6902f4f72fa932445feb5a0488c441acaadcb606
Sarah Carter <sarah.c@fdak.net>
2020-04-18 18:28:20 -0800

    Write Base Class with two functions, one virtual

--- Labs/Lab12/Lab12/main.cpp
+++ Labs/Lab12/Lab12/main.cpp
@@ -1,15 +1,37 @@
 //
 //  main.cpp
 //  Lab12
-//
-//  Created by Sarah Carter on 4/18/20.
-//  Copyright © 2020 Sarah Carter. All rights reserved.
+//  Sarah Carter
+//  CS202
 //
 
 #include <iostream>
 
+//[ ] Write Base Class with two functions, one virtual
+//[ ] Write Derived Class with two functions, one virtual
+//[ ] Test Base pointer to Base class object
+//[ ] Test Bese pointer to Derived class object
+//[ ] Test Derived pointer to Derived class object
+//[ ] Test Base class reference to Base class object
+//[ ] Test Base class reference to Derived class object
+//[ ] Test Derived class reference to Derived class object
+
+class Base {
+public:
+    Base() { std::cout << "Base::Base constructor\n"; }
+    ~Base() { std::cout << "Base::~Base destructor\n"; }
+    
+    void function1() { std::cout << "Base::function1() - non virtual\n"; }
+    virtual void function2() { std::cout << "Base::function2() - virtual\n"; }
+};
+
+void printDivider() {
+    std::cout << "---------------------------\n";
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    { Base b; b.function1(); b.function2(); }
+    printDivider();
+    
     return 0;
 }
