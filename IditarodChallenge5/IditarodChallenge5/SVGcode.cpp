ade617b7fbe06c354973b76877ce913e33078fca
Sarah Carter <sarah.c@fdak.net>
2020-04-19 21:43:13 -0800

    Create header and source files for SVG

---
+++ IditarodChallenge5/IditarodChallenge5/SVGcode.cpp
@@ -0,0 +1,9 @@
+//
+//  SVGcode.cpp
+//  IditarodChallenge5
+//
+//  Created by Sarah Carter on 4/19/20.
+//  Copyright © 2020 Sarah Carter. All rights reserved.
+//
+
+#include "SVGcode.hpp"
---
+++ IditarodChallenge5/IditarodChallenge5/SVGcode.hpp
@@ -0,0 +1,16 @@
+//
+//  SVGcode.hpp
+//  IditarodChallenge5
+//
+//  Created by Sarah Carter on 4/19/20.
+//  Copyright © 2020 Sarah Carter. All rights reserved.
+//
+
+#ifndef SVGcode_hpp
+#define SVGcode_hpp
+
+#include <stdio.h>
+
+#include "CityNode.hpp"
+
+#endif /* SVGcode_hpp */
