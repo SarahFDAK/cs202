4c0a94490c4e4ecb80983ad2baf12946c9f91949
Sarah Carter <sarah.c@fdak.net>
2020-04-19 20:21:15 -0800

    Write wrapper template class

--- Labs/Lab13/Lab13/Lab13-main.cpp
+++ Labs/Lab13/Lab13/Lab13-main.cpp
@@ -1,15 +1,25 @@
 //
-//  main.cpp
-//  Lab13
-//
-//  Created by Sarah Carter on 4/19/20.
-//  Copyright © 2020 Sarah Carter. All rights reserved.
+//  Lab13: Class Templates
+//  Sarah Carter
+//  CS202
 //
 
 #include <iostream>
 
+//[ ] Write wrapper template class
+//[ ] Write friend override operator << to work with wrapper
+
+template <typename WrapType>
+class Wrapper {
+public:
+    Wrapper(const WrapType& w) : obj_(w) {}
+    
+private:
+    WrapType obj_;
+};
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    Wrapper<int> {50};
+    Wrapper<std::string> {"How many years I feel like I've been in isolation."};
     return 0;
 }
