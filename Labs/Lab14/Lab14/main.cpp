c046d980012653aac6b32b59229ed62a357c4ac3
Sarah Carter <sarah.c@fdak.net>
2020-04-23 15:46:30 -0800

    Fix it to work with a C string

--- Labs/Lab14/Lab14/main.cpp
+++ Labs/Lab14/Lab14/main.cpp
@@ -10,18 +10,23 @@
 
 //[x] Write a function template 'twice'
 //[x] Test it with an int and a double
-//[ ] Test iwth with a C++ string
-//[ ] Fix it to work with a C string
+//[x] Test iwth with a C++ string
+//[x] Fix it to work with a C string
 
 template <typename T>
 T twice(const T& x){
     return x + x;
 }
 
+std::string twice(const char* x){
+    return twice(std::string(x));
+}
+
 int main(int argc, const char * argv[]) {
     std::cout << twice(2) << std::endl;
     std::cout << twice(4.6) << std::endl;
-    std::cout << twice(std::string("Vincent")) << std::endl;
+    std::cout << twice(std::string("Vincent Twice")) << std::endl;
+    std::cout << twice("Vincent Twice") << std::endl;
     
     return 0;
 }
