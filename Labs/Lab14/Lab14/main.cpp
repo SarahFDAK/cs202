b02a27d3b5f02e377b851c20eb8439878e44c69e
Sarah Carter <sarah.c@fdak.net>
2020-04-23 15:43:40 -0800

    Test it with an int and a double

--- Labs/Lab14/Lab14/main.cpp
+++ Labs/Lab14/Lab14/main.cpp
@@ -8,7 +8,7 @@
 
 #include <iostream>
 
-//[ ] Write a function template 'twice'
+//[x] Write a function template 'twice'
 //[ ] Test it with an int and a double
 //[ ] Test iwth with a C++ string
 //[ ] Fix it to work with a C string
@@ -19,6 +19,8 @@ T twice(const T& x){
 }
 
 int main(int argc, const char * argv[]) {
+    std::cout << twice(2) << std::endl;
+    std::cout << twice(4.6) << std::endl;
     
     return 0;
 }
