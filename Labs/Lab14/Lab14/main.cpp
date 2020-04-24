af201cba2ae63e71c7c3afd43a779387abcebc09
Sarah Carter <sarah.c@fdak.net>
2020-04-23 17:02:39 -0800

    Write normalize and length functions for TVector3 class

--- Labs/Lab14/Lab14/main.cpp
+++ Labs/Lab14/Lab14/main.cpp
@@ -7,13 +7,15 @@
 //
 
 #include <iostream>
+#include <cmath>
 
 //[x] Write a function template 'twice'
 //[x] Test it with an int and a double
 //[x] Test iwth with a C++ string
 //[x] Fix it to work with a C string
 //[x] Write a TVector3 class that uses template function for operator<<
-//[ ] Write dot, cross, normalize and length functions for TVector3 class
+//[x] Write normalize and length functions for TVector3 class
+//[ ] Write dot, cross  functions for TVector3 class
 
 template <typename T>
 T twice(const T& x){
@@ -33,12 +35,32 @@ public:
     
     TVector3() {}
     TVector3(T x, T y, T z) : x(x), y(y), z(z) {}
+    
+    TVector3& operator *=(T s) {
+        x *= s;
+        y *= s;
+        z *= s;
+        return *this;
+    }
 };
 
 template<typename T>
 std::ostream& operator<<(std::ostream& os, const TVector3<T>& v){
-    os << "(" << v.x << ", " << v.y << ", " << v.z << ")" << std::endl;
+    os << "(" << v.x << ", " << v.y << ", " << v.z << ")";
     return os;
+}
+
+template <typename T>
+T length(TVector3<T> v) {
+    return std::sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+}
+
+template <typename T>
+TVector3<T> normalize(TVector3<T> v){
+    T l = length(v);
+    TVector3<T> result = v;
+    v *= 1 / l;
+    return v;
 }
 
 int main(int argc, const char * argv[]) {
@@ -47,7 +69,11 @@ int main(int argc, const char * argv[]) {
     std::cout << twice(std::string("Vincent Twice")) << std::endl;
     std::cout << twice("Vincent Twice") << std::endl;
     
-    TVector3<float> v1;
+    TVector3<float> v1 { 1, 2, 3 };
+    auto v2 = normalize(v1);
     std::cout << v1 << std::endl;
+    std::cout << "Length of v1 = " << length(v1) << std::endl;
+    std::cout << "Normalize v1 = " << v2 << std::endl;
+    std::cout << "Length of v2 = " << length(v2) << std::endl;
     return 0;
 }
