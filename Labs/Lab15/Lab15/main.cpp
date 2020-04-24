948fe091f8bb3761377a82e73f41064998ec5dd9
Sarah Carter <sarah.c@fdak.net>
2020-04-23 18:36:13 -0800

    Test STL algorithm from algorithm header

--- Labs/Lab15/Lab15/main.cpp
+++ Labs/Lab15/Lab15/main.cpp
@@ -7,9 +7,24 @@
 //
 
 #include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+
+//[x] Test STL algorithm from algorithm header
+//[ ] Test STL algorithm from numeric header
+
+void copyBackward() {
+    std::vector<int> first { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    std::vector<int> copy(first.size());
+    std::copy_backward(first.begin(), first.end(), copy.end());
+    for(auto& p: copy)
+        std::cout << p << " ";
+    std::cout << std::endl;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    copyBackward();
+    
     return 0;
 }
