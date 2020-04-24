c7bf351e4cc49b67081ed5c3e8fb5be26184a4d7
Sarah Carter <sarah.c@fdak.net>
2020-04-23 17:17:55 -0800

    Write dot, cross  functions for TVector3 class

--- Labs/Lab14/Lab14/main.cpp
+++ Labs/Lab14/Lab14/main.cpp
@@ -15,7 +15,7 @@
 //[x] Fix it to work with a C string
 //[x] Write a TVector3 class that uses template function for operator<<
 //[x] Write normalize and length functions for TVector3 class
-//[ ] Write dot, cross  functions for TVector3 class
+//[x] Write dot, cross  functions for TVector3 class
 
 template <typename T>
 T twice(const T& x){
@@ -63,6 +63,20 @@ TVector3<T> normalize(TVector3<T> v){
     return v;
 }
 
+template <typename T, typename U>
+auto dot(TVector3<T> a, TVector3<U> b){
+    return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+template <typename T, typename U>
+auto cross(TVector3<T> a, TVector3<U> b) {
+    return TVector3<decltype(a.x * b.x)>{
+        a.y * b.z - a.z * b.y, //x
+        a.z * b.x - a.x * b.z, //y
+        a.x * b.y - a.y * b.x  //z
+    };
+}
+
 int main(int argc, const char * argv[]) {
     std::cout << twice(2) << std::endl;
     std::cout << twice(4.6) << std::endl;
@@ -75,5 +89,13 @@ int main(int argc, const char * argv[]) {
     std::cout << "Length of v1 = " << length(v1) << std::endl;
     std::cout << "Normalize v1 = " << v2 << std::endl;
     std::cout << "Length of v2 = " << length(v2) << std::endl;
+    
+    TVector3<double> up { 0, 1, 0 };
+    TVector3<double> right { 1, 0, 0 };
+    std::cout << "Dot(up, right)   = " << dot(up, right) << std::endl;
+    std::cout << "Cross(up, right) = " << cross(up, right) << std::endl;
+    std::cout << "Cross(right, up) = " << cross(right, up) << std::endl;
+    double angle1 = dot(right, v2) * 180.0 / 3.14159;
+    std::cout << "Angle between right and v2 = " << angle1 << std::endl;
     return 0;
 }
