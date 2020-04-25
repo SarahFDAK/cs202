2d3a9f5ab293cc30032029e26f7dd30aef79d49f
Sarah Carter <sarah.c@fdak.net>
2020-04-25 12:34:17 -0800

    Import CityNode and SVG header and source files into IC7

---
+++ IditarodChallenge7/IditarodChallenge7/CityNode.hpp
@@ -0,0 +1,121 @@
+//
+//  CityNode.hpp
+//  IditarodChallenge3
+//
+//  Created by Sarah Carter on 4/9/20.
+//  Copyright © 2020 Sarah Carter. All rights reserved.
+//
+
+#ifndef CityNode_hpp
+#define CityNode_hpp
+
+#include <stdio.h>
+#include <vector>
+#include <iostream>
+#include <istream>
+#include <fstream>
+#include <sstream>
+
+
+class CityNode {
+public:
+    CityNode ();
+    
+    void setCityNode(int num, double latitude, double longitude);
+    
+    int getNodeNum() const;
+    
+    double getNodeLat() const;
+    
+    double getNodeLong() const;
+    
+    ~CityNode ();
+private:
+    int nodeNum_;
+    double nodeLat_;
+    double nodeLong_;
+    double graphX_;
+    double graphY_;
+};
+
+class CityList {
+public:
+    CityList();
+    
+    void fillList(CityNode& node);
+    
+    bool checkFile(std::istream& fin);
+    
+    void readFile(std::istream& fin, CityNode& node);
+    
+    CityNode getCityNode(const int index);
+    
+    double distance(int first, int second) const;
+    
+    int getCityVectorCount() const;
+    
+    double getMaxLat();
+    
+    double getMinLat();
+    
+    double getMinLong();
+    
+    double getMaxLong();
+    
+    ~CityList();
+private:
+    std::vector<CityNode> cities_;
+};
+
+class CityPath{
+public:
+    CityPath();
+    
+    void fillPath(const int list);
+    
+    void emptyPath();
+        
+    void deleteUsed(int added);
+    
+    int getPathSize() const;
+    
+    int getPathEntry(const int entryNum);
+    
+    void sectionCount(int listLength);
+        
+    bool getPathDup(const int city) const;
+    
+    std::vector<int>& getPathVector();
+    
+    ~CityPath();
+private:
+    std::vector<int> path_;
+};
+
+class TSPSolver {
+public:
+    TSPSolver();
+    
+    void setBestList(const std::vector<int>& list);
+    void setBestDist(const double dist);
+    
+    double getBestDist() const;
+    void showBestList() const;
+    
+    int getRandomInt(const int count) const;
+        
+    void fillVector(CityPath& newPath);
+    
+    void SolveRandomly(CityList& cList, const int M, CityPath& newPath);
+    
+    void SolveGreedy(CityList& cList, CityPath& newPath);
+    
+    void SolveMyWay(CityList& cList, CityPath& newPath);
+    
+    ~TSPSolver();
+private:
+    std::vector<int> bestList_;
+    double bestDist_;
+};
+
+#endif /* CityNode_hpp */
---
+++ IditarodChallenge7/IditarodChallenge7/SVGcode.hpp
@@ -0,0 +1,28 @@
+//
+//  SVGcode.hpp
+//  IditarodChallenge5
+//
+//  Created by Sarah Carter on 4/19/20.
+//  Copyright © 2020 Sarah Carter. All rights reserved.
+//
+
+#ifndef SVGcode_hpp
+#define SVGcode_hpp
+
+#include <stdio.h>
+
+#include "CityNode.hpp"
+
+double convertPoint(const double v, double vmax, double vmin, double dimension1, double dimension2);
+
+std::string ChartPath(CityList& list, CityPath& bestPath, double xmin, double xmax, double ymin, double ymax);
+
+std::string ChartPoints(CityList& list, CityPath& bestPath, double xmin, double xmax, double ymin, double ymax);
+
+std::string buildSVG(const std::string& chartPath, const std::string& chartPoints, double width, double height);
+
+bool CreateFile(const std::string& svgData, const std::string& title);
+
+void inProgress(CityList& list, CityPath& bestPath, CityPath& tempPath);
+
+#endif /* SVGcode_hpp */
