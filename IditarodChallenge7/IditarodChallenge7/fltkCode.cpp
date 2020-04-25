13d55c8e30c636551ee48db0121e071569f8ed57
Sarah Carter <sarah.c@fdak.net>
2020-04-25 12:40:18 -0800

    Create FLTK header and source files

---
+++ IditarodChallenge7/IditarodChallenge7/fltkCode.cpp
@@ -0,0 +1,9 @@
+//
+//  fltkCode.cpp
+//  IditarodChallenge7
+//
+//  Created by Sarah Carter on 4/25/20.
+//  Copyright © 2020 Sarah Carter. All rights reserved.
+//
+
+#include "fltkCode.hpp"
---
+++ IditarodChallenge7/IditarodChallenge7/fltkCode.hpp
@@ -0,0 +1,21 @@
+//
+//  fltkCode.hpp
+//  IditarodChallenge7
+//
+//  Created by Sarah Carter on 4/25/20.
+//  Copyright © 2020 Sarah Carter. All rights reserved.
+//
+
+#ifndef fltkCode_hpp
+#define fltkCode_hpp
+
+#include <stdio.h>
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Widget.H>
+
+Fl_Window* CreateWindow();
+
+Fl_Window* PopupWindow();
+
+#endif /* fltkCode_hpp */
