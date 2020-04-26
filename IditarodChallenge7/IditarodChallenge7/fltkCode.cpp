6ccffea388d14c38b691e404cd3d5c28fa6af124
Sarah Carter <sarah.c@fdak.net>
2020-04-25 17:05:26 -0800

    Create basic GUI (window and exit button)

--- IditarodChallenge7/IditarodChallenge7/fltkCode.cpp
+++ IditarodChallenge7/IditarodChallenge7/fltkCode.cpp
@@ -6,4 +6,25 @@
 //  Copyright © 2020 Sarah Carter. All rights reserved.
 //
 
+#include <FL/Fl_Button.H>
+#include <FL/Fl_Native_File_Chooser.H>
+#include <FL/Fl_Input.H>
+#include <FL/Fl_Output.H>
+#include <FL/Fl_Box.H>
+
 #include "fltkCode.hpp"
+
+Fl_Button* quit = nullptr;
+
+Fl_Window* CreateWindow(){
+    Fl_Window* win = new Fl_Window(800, 400, "Traveling Salesperson Problem");
+    win->begin();
+    
+    quit = new Fl_Button(350, 350, 100, 25, "Quit");
+    quit->color(FL_MAGENTA);
+    quit->labelfont(FL_BOLD);
+    quit->labelfont(20);
+    
+    win->end();
+    return win;
+}
--- IditarodChallenge7/IditarodChallenge7/main.cpp
+++ IditarodChallenge7/IditarodChallenge7/main.cpp
@@ -8,8 +8,12 @@
 
 #include <iostream>
 
+#include "fltkCode.hpp"
+#include "SVGcode.hpp"
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    Fl_Window* program = CreateWindow();
+    
+    program->show();
+    return Fl::run();
 }
