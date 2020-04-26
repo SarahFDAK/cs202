1a5ac2e89f423e06c322fe8a659de1e536bc238e
Sarah Carter <sarah.c@fdak.net>
2020-04-26 10:52:32 -0800

    Create FLTK header and source files

---
+++ IditarodChallenge8/IditarodChallenge8/fltkCode.cpp
@@ -0,0 +1,9 @@
+//
+//  fltkCode.cpp
+//  IditarodChallenge8
+//
+//  Created by Sarah Carter on 4/26/20.
+//  Copyright © 2020 Sarah Carter. All rights reserved.
+//
+
+#include "fltkCode.hpp"
---
+++ IditarodChallenge8/IditarodChallenge8/fltkCode.hpp
@@ -0,0 +1,14 @@
+//
+//  fltkCode.hpp
+//  IditarodChallenge8
+//
+//  Created by Sarah Carter on 4/26/20.
+//  Copyright © 2020 Sarah Carter. All rights reserved.
+//
+
+#ifndef fltkCode_hpp
+#define fltkCode_hpp
+
+#include <stdio.h>
+
+#endif /* fltkCode_hpp */
