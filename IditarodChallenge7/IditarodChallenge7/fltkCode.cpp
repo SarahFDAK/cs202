9bc3115644fd6f521a6143ca4840341865c43904
Sarah Carter <sarah.c@fdak.net>
2020-04-25 17:28:00 -0800

    Add buttons for Browse, Random, Greedy, and Sorted

--- IditarodChallenge7/IditarodChallenge7/fltkCode.cpp
+++ IditarodChallenge7/IditarodChallenge7/fltkCode.cpp
@@ -15,10 +15,19 @@
 #include "fltkCode.hpp"
 
 Fl_Button* quit = nullptr;
+Fl_Button* browse = nullptr;
+Fl_Button* randomly = nullptr;
+Fl_Button* greedy = nullptr;
+Fl_Button* sorted = nullptr;
 
 Fl_Window* CreateWindow(){
     Fl_Window* win = new Fl_Window(800, 400, "Traveling Salesperson Problem");
     win->begin();
+    
+    browse = new Fl_Button(30, 150, 100, 20, "Browse Files");
+    randomly = new Fl_Button(212, 180, 125, 20, "Solve Randomly");
+    greedy = new Fl_Button(340, 180, 125, 20, "Solve Greedy");
+    sorted = new Fl_Button(468, 180, 125, 20, "Solve Sorted");
     
     quit = new Fl_Button(350, 350, 100, 25, "Quit");
     quit->color(FL_MAGENTA);
