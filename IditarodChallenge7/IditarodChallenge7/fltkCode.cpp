e5f2baa8d815c9d1554217c5ad5d1f13ef96aaab
Sarah Carter <sarah.c@fdak.net>
2020-04-25 17:36:01 -0800

    Write Browse callback and tie to browse button

--- IditarodChallenge7/IditarodChallenge7/fltkCode.cpp
+++ IditarodChallenge7/IditarodChallenge7/fltkCode.cpp
@@ -13,6 +13,7 @@
 #include <FL/Fl_Box.H>
 
 #include "fltkCode.hpp"
+#include "SVGcode.hpp"
 
 Fl_Button* quit = nullptr;
 Fl_Button* browse = nullptr;
@@ -20,6 +21,24 @@ Fl_Button* randomly = nullptr;
 Fl_Button* greedy = nullptr;
 Fl_Button* sorted = nullptr;
 Fl_Output* fileChoice = nullptr;
+
+std::string userFile;
+
+void browseClicked(Fl_Widget*, void* data){
+    Fl_Native_File_Chooser fileFind;
+    fileFind.title("Select File to Analyze");
+    fileFind.type(Fl_Native_File_Chooser::BROWSE_FILE);
+    switch( fileFind.show() ){
+        case -1: fprintf(stderr, "ERROR: %s\n", fileFind.errmsg()); break;    // ERROR
+        case  1: fprintf(stderr, "*** CANCEL\n"); break;        // CANCEL
+        default:                                 // PICKED FILE
+            fileChoice->value(fileFind.filename());
+          break;
+    }
+    std::string choice = fileChoice->value();
+    std::istringstream is(choice);
+    is >> userFile;
+}
 
 Fl_Window* CreateWindow(){
     Fl_Window* win = new Fl_Window(800, 400, "Traveling Salesperson Problem");
@@ -31,6 +50,8 @@ Fl_Window* CreateWindow(){
     randomly = new Fl_Button(212, 180, 125, 20, "Solve Randomly");
     greedy = new Fl_Button(340, 180, 125, 20, "Solve Greedy");
     sorted = new Fl_Button(468, 180, 125, 20, "Solve Sorted");
+    
+    browse->callback(browseClicked);
     
     quit = new Fl_Button(350, 350, 100, 25, "Quit");
     quit->color(FL_MAGENTA);
